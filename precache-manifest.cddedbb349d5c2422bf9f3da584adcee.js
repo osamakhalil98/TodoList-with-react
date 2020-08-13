9892604c4e9fe4e8f85083e51926081e134d74d7
osamakhalil98 <mozzbozz35@gmail.com>
2020-08-13 14:23:14 +0200

    Updates

---
+++ precache-manifest.cddedbb349d5c2422bf9f3da584adcee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d01e250cc51b0debb780a7973d3e680",
+    "url": "/TodoList-with-react/index.html"
+  },
+  {
+    "revision": "16003b8e4ee5b055e153",
+    "url": "/TodoList-with-react/static/css/main.a05a89f1.chunk.css"
+  },
+  {
+    "revision": "e49ce7c33ba9f951dc88",
+    "url": "/TodoList-with-react/static/js/2.4fde0391.chunk.js"
+  },
+  {
+    "revision": "16003b8e4ee5b055e153",
+    "url": "/TodoList-with-react/static/js/main.ed9f91be.chunk.js"
+  },
+  {
+    "revision": "ab4e45efaeeb5c29f826",
+    "url": "/TodoList-with-react/static/js/runtime-main.0b6289f4.js"
+  }
+]);
